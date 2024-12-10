ec0fd3d9a03acbee5339383cdae2b0354b55208f
flasapp <flasap@gmail.com>
2024-12-10 13:59:12 -0300

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+/** @type {import('@sveltejs/kit').Config} */
 
-export default {
-  kit: {
-    adapter: vercel({
-      runtime: 'nodejs20.x'
-    })
-  }
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
+
+export default config;
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path, { resolve } from 'path'
+import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		enhancedImages(),
+		sveltekit()
+	],
+	resolve: {
+		// alias: {
+		//   "@": fileURLToPath(new URL("./src", import.meta.url)),
+		//   "./runtimeConfig": "./runtimeConfig.browser",
+		// },
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			"./runtimeConfig": "./runtimeConfig.browser",
+		},
+	},
+	server: { port: 3030}
 });
