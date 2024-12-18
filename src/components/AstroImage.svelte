a0f4524bfa3aa66d46168188b8bd5b97d6126f28
flasapp <flasap@gmail.com>
2024-12-18 13:59:55 -0300

    update

--- src/components/AstroImage.svelte
+++ src/components/AstroImage.svelte
@@ -13,24 +13,30 @@
 		top: 80px;
 		right: 120px;
 		animation: float 12s linear infinite; /* Más lento y uniforme */
+		filter: brightness(1.8) contrast(1);
 	}
 	/* Animation like the austronaut is loating going up and down */
 	
 	@keyframes float {
 	0% {
 		transform: translate(0, 0) rotate(0deg);
+		filter: brightness(1.2);
 	}
 	25% {
-		transform: translate(30px, -30px) rotate(10deg); /* Arriba a la derecha y un poco de rotación */
+		transform: translate(30px, -30px) rotate(10deg);
+		filter: brightness(1.2.6);
 	}
 	50% {
-		transform: translate(60px, 0) rotate(0deg); /* Abajo a la derecha, vuelve al centro */
+		transform: translate(60px, 0) rotate(0deg);
+		filter: brightness(1.8);
 	}
 	75% {
-		transform: translate(30px, 30px) rotate(-10deg); /* Abajo a la izquierda y rota al otro lado */
+		transform: translate(30px, 30px) rotate(-10deg);
+		filter: brightness(1.6);
 	}
 	100% {
-		transform: translate(0, 0) rotate(0deg); /* Arriba a la izquierda, vuelve al inicio */
+		transform: translate(0, 0) rotate(0deg);
+		filter: brightness(1.2);
 	}
 }
 
